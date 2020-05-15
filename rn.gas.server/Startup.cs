43ebfffe77856f193fdb329c9ee64277480022ae
Fulvio Infante <fulvio.infante@system-data.it>
2020-05-15 16:58:46 +0200

    fine giornata

--- rn.gas.server/Startup.cs
+++ rn.gas.server/Startup.cs
@@ -27,6 +27,7 @@ namespace rn.gas.server
       public IConfiguration Configuration { get; }
 
       // This method gets called by the runtime. Use this method to add services to the container.
+      [Obsolete]
       public void ConfigureServices(IServiceCollection services)
       {
          var connectionString = Configuration.GetConnectionString("defaultConnection");
